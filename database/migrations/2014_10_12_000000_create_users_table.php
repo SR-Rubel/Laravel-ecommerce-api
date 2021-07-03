814d0c3e6fed5a95f03fbe41f7a37acf9d61e3da
SR-Rubel <mdrubelh739@gmail.com>
2021-07-03 12:02:32 +0600

    role added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('phone_no');
+            $table->integer('isadmin')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
