f4722c4050ddd85787fdf7af48f2d92ff3828b6e
SR-Rubel <rubel162765@gmail.com>
2021-12-02 18:37:56 +0600

    heroku 2

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Traits\JsonResponser;
+use App\Traits\jsonResponser as TraitsJsonResponser;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +10,5 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests,JsonResponser;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests,TraitsJsonResponser;
 }
--- app/traits/JsonResponser.php
+++ app/traits/JsonResponser.php
@@ -4,7 +4,7 @@ namespace App\Traits;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
-trait JsonResponser
+trait jsonResponser
 {
     // this function will return json success response
     private function successResponse($data,$code=200){
