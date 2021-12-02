3acf47b76f5ada5c9ea6c70c0b54162ca3c68390
SR-Rubel <rubel162765@gmail.com>
2021-12-02 19:07:46 +0600

    heroku 8

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-
-use App\Traits\jsonResponser as TraitsJsonResponser;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,5 +10,46 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests,TraitsJsonResponser;
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    private function successResponse($data,$code=200){
+        return response()->json($data,$code);
+    }
+
+    //this will return single instance json response
+    protected function oneResponse(Model $instance, $code=200)
+    {
+        $transformer=$instance->transformer;
+        $instance=$this->transformData($instance,$transformer);
+
+        return $this->successResponse($instance,$code);
+    }
+
+    //this will return multiple instance json response
+    protected function mutliResponse(Collection $collection,$code=200)
+    {
+        if($collection->isEmpty()){
+            return $this->successResponse(["data"=>$collection],$code);
+        }
+
+        $transformer=$collection->first()->transformer;
+        $collection=$this->transformData($collection,$transformer);
+        return $this->successResponse(["data"=>$collection],$code);
+    }
+    // this will return error response with http error code
+    protected function errorResponse($msg,$code)
+    {
+        return response()->json(['error'=>$msg,'code'=>$code],$code);
+    }
+
+    // this will return custom response
+    protected function customResponse($msg_or_data,$code=200)
+    {
+        return response()->json($msg_or_data,$code);
+    }
+
+    protected function transformData($data,$transformer)
+    {
+        $transformation=fractal($data,new $transformer);
+        return $transformation->toArray();
+    }
 }
